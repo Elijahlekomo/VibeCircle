c59c6e8d1211b346fd6c28557d750e57be172b5a
Elijah Lekomo <lekomoayomide@gmail.com>
2025-03-23 23:34:03 +0200

    Finished working on login screen Changed splash screen color

--- Controls/Spacer.cs
+++ Controls/Spacer.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace VibeCircle.Controls
 {
-    class Spacer
+    public class Spacer : ContentView
     {
+        public BoxView Box { get; set; }
+
+        public Spacer() 
+        {
+            Box = new BoxView
+            {
+                BackgroundColor = Colors.Transparent,
+                HeightRequest = 20
+            };
+
+            Content = Box;
+        }
+        public static readonly BindableProperty SpaceProperty =
+            BindableProperty.Create(nameof(Space), typeof(int), typeof(Spacer), defaultValue: 20, propertyChanged: OnSpacePropertyChanged);
+
+        public int Space 
+        {  
+            get => (int)GetValue(SpaceProperty); 
+            set => SetValue(SpaceProperty, value);
+        }
+
+        private static void OnSpacePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if(bindable is Spacer spacer)
+            {
+                spacer.Box.HeightRequest = spacer.Space;
+            }
+        }
     }
 }
